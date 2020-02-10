aeb6d290ebbebd1f2cefa408d04eab4c31cf9e26
Stricks1 <gmalheiross@gmail.com>
2020-02-10 11:54:05 -0300

    startNativeTeste

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-//start index
-<body>
-    <Text>Test</Text>
-</body>
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
